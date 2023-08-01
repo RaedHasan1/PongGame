43c5cd482fa05a97e2cdd9086519d64c8f42286b
Raed Hasan <136272243+RaedHasan1@users.noreply.github.com>
2023-08-01 19:41:49 +0300

    computer.py

--- PongGame/computer.py
+++ PongGame/computer.py
@@ -5,8 +5,8 @@ class Computer(Paddle):
     def __init__(self):
         super().__init__()
         self.color("blue")
-
-    def computer_moving(self, ball):
+    @overriding
+    def move_update(self):
         if self.ycor() >= 0 and ball.ycor() < 0:
             self.goto(self.xcor(), self.ycor() - 10)
         elif self.ycor() <= 0 and ball.ycor() > 0:
