f260205c217128a2e57fa89b8fccd34cb4b644b1
Raed Hasan <136272243+RaedHasan1@users.noreply.github.com>
2023-08-01 19:38:23 +0300

    ball.py

--- PongGame/ball.py
+++ PongGame/ball.py
@@ -15,11 +15,8 @@ class Ball(Turtle):
         self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
 
     def y_bounce(self):
-        if self.ycor() > 0:
-            self.y_move *= -1
-        else:
-            self.y_move *= -1
-
+        self.ycor() > 0 ? self.y_move *= -1 : self.y_move *= -1
+        
     def x_bounce(self):
         self.x_move *= -1
         self.y_move *= -1
